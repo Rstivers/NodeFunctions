9da8ec7e755e18aa139fe300461cc76453ebad02
Rstivers <ryan.a.stivers@gmail.com>
2017-05-26 02:59:38 -0400

    Update update.js

--- node/update.js
+++ node/update.js
@@ -7,9 +7,9 @@ var url = "mongodb://localhost:27017/mydb";
 MongoClient.connect(url, function(err,db) {
   if (err) throw err;
   //select field to update
-  var query = { character: 'Bepis' };
+  var query = { name: '' };
   //new values to be updated
-  var newvalues = { name: 'Bitchboy', email: 'fuck@gmail.com', character: 'Pebis'};
+  var newvalues = { name: '', email: '', character: ''};
   db.collection("members").update(query, newvalues, function(err, res) {
     if (err) throw err;
     console.log(res.result.nModified + " record updated");
